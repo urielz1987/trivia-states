06cd265b2a1f05b602c41e899ba3d34df92015b1
Uriel Zohar <urielfed@gmail.com>
2018-11-13 21:03:08 +0200

    adding fontawsome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './components/question/question.component';
