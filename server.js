dc4c0380c660be0ab474b56511636d65c16b3389
Uriel Zohar <urielfed@gmail.com>
2018-12-04 22:32:35 +0200

    server index html

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/trivia-states'));
 
 app.listen(process.env.PORT || 8080);
 
 // PathLocationStrategy
 
 app.get('*', function (req, res) {
-    res.status(200).sendFile(path.join(__dirname + '/dist/index.html'));
+    res.status(200).sendFile(path.join(__dirname + '/dist/trivia-states/index.html'));
 });
 
-console.log('Console listening!');
+console.log('Console listening to ' + (process.env.PORT || 8080));
