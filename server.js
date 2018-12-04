42dc6e371659f539be120d5fe13e03af43a27316
Uriel Zohar <urielfed@gmail.com>
2018-12-04 22:11:51 +0200

    server wildcard

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.listen(process.env.PORT || 8080);
 
 // PathLocationStrategy
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('*', function (req, res) {
+    res.status(200).sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 console.log('Console listening!');
